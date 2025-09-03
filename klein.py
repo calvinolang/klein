89eec547bcfb5969d1441b55a1cd1f682e62bf34
calvinolang <t.anzidei@gmail.com>
2025-09-03 21:00:21 +0200

    Added the preprocessor

--- klein.py
+++ klein.py
@@ -1,7 +1,18 @@
-def main():
-    # your main program logic here
-    print("Hello, world!")
+from fileinput import filename
+import sys
+import subprocess
 
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: python klein.py <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    preprocessorReturnCode = preprocess(filename)  
+    sys.exit(preprocessorReturnCode)
+    
+def preprocess(filename):
+    return subprocess.run(["gcc", "-E", "-P", filename, "-o", f"{filename}.i"], check=True).returncode
 
 if __name__ == "__main__":
     main()
